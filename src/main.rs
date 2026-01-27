56c4f8f60c8c6302ec364f7bbdb7b9e4a021a308
WASDetchan <yarik.antipov2008@gmail.com>
2026-01-27 20:21:45 +0300

    refactor headers

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::sync::LazyLock;
 
 use anyhow::Context;
 use clap::Parser;
-use reqwest::Client;
+use reqwest::{
+    Client,
+    header::{HeaderMap, HeaderValue},
+};
 
 const YANDEX_MUSIC_API_URL: &str = "https://api.music.yandex.ru:443";
 
@@ -21,14 +24,33 @@ static CLIENT: LazyLock<Client> = LazyLock::new(|| {
         .unwrap()
 });
 
-async fn get_user_id(token: &str) -> anyhow::Result<String> {
+fn make_shared_headers(token: &str) -> HeaderMap<HeaderValue> {
+    let mut map = HeaderMap::new();
+    map.insert(
+        "x-Yandex-Music-Client",
+        HeaderValue::from_static("YandexMusicAndroid/24024312"),
+    );
+    map.insert("User-Agent", HeaderValue::from_static("okhttp/4.12.0"));
+    map.insert(
+        "Authorization",
+        HeaderValue::from_str(format!("OAuth {token}").as_str()).unwrap(),
+    );
+    map.insert("accept", HeaderValue::from_static("application/json"));
+
+    map
+}
+
+// struct AccountStatus {
+//     result
+// }
+
+async fn get_user_id(headers: HeaderMap<HeaderValue>) -> anyhow::Result<String> {
     CLIENT
         .get(format!("{YANDEX_MUSIC_API_URL}/account/status"))
+        .headers(headers)
         .header("x-Yandex-Music-Client", "YandexMusicAndroid/24024312")
-        .header("User-Agent", "okhttp/4.12.0")
         // .header("Content-Type", "x-www-form-urlencoded")
         .header("accept", "application/json")
-        .header("Authorization", format!("OAuth {token}"))
         .send()
         .await
         .context("Getting user id")?
@@ -59,15 +81,17 @@ async fn get_likes(token: &str, user_id: &str) -> anyhow::Result<String> {
 async fn main() -> anyhow::Result<()> {
     let Args { token } = Args::parse();
 
-    // let user_id = get_user_id(&token).await?;
+    let headers = make_shared_headers(&token);
 
-    // println!("{user_id}");
+    let user_id = get_user_id(headers).await?;
 
-    let user_id = String::from("000000000");
-
-    let likes = get_likes(&token, &user_id).await?;
-
-    println!("{likes}");
+    println!("{user_id}");
+    //
+    // let user_id = String::from("000000000");
+    //
+    // let likes = get_likes(&token, &user_id).await?;
+    //
+    // println!("{likes}");
 
     Ok(())
 }
